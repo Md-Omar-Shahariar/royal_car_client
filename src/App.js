9a3d95a9e2b4aef9bfa599c9490ff68276ca75cb
Md-Omar-Shahariar <osafridi00@gmail.com>
2022-05-06 21:59:31 +0600

    connect with server

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/apple")
+      .then((res) => res.json())
+      .then((d) => setData(d));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>
+        {data.map((d) => (
+          <p key={d.name}>{d.name}</p>
+        ))}
+      </h2>
     </div>
   );
 }
